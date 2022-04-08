02404c52fab5ecad3466d5a109276724034e01be
Colin MacGiollaEain <colin@arista.com>
2022-04-08 14:23:22 +0100

    Whitespace tidyup

--- ansible_collections/arista/cvp/plugins/module_utils/change_tools.py
+++ ansible_collections/arista/cvp/plugins/module_utils/change_tools.py
@@ -584,7 +584,7 @@ class CvChangeControlTools():
             except Exception:
                 MODULE_LOGGER.error('Change control with id %s not found' % cc_id)
                 change = None
-                
+
 
         return change
 
@@ -655,12 +655,11 @@ class CvChangeControlTools():
         elif state == "set" and self.__check_mode is False:
             changeControl = CvpChangeControlBuilder()
             changeControl.add_known_uuid([v[1] for v in self.__cc_index])
-            
+
             # Check that our generated CCID is not already in use, and if it is,
             # add the UUID to the known list, and run again
             while True:
                 cc_structure = changeControl.build_cc(change, name)
-                
                 if self.get_change_control(cc_structure['key']) is None:
                     break
                 else:
